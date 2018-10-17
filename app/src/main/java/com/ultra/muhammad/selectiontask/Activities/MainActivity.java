b736826c3db0bd6d51e2a22e99ba028570150e98
MuhammadElsayed96 <muhammadelsayed96@yahoo.com>
2018-10-17 05:20:43 +0200

    The Task is read

--- app/src/main/java/com/ultra/muhammad/selectiontask/Activities/MainActivity.java
+++ app/src/main/java/com/ultra/muhammad/selectiontask/Activities/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ultra.muhammad.selectiontask.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,13 +11,18 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.hbb20.CountryCodePicker;
 import com.ultra.muhammad.selectiontask.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String PREF_COUNTRY_CODE = "pref_country_code";
+    private static final String PREF_FILE_NAME = "soleeklab_pref";
+
     private Button mSignOutButton;
     private FirebaseAuth mAuth;
+    private CountryCodePicker codePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +38,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initViews() {
         Log.d(TAG, "initViews() has been instantiated!");
         mSignOutButton = findViewById(R.id.sign_out_button);
+        codePicker = findViewById(R.id.country_picker_view);
     }
 
     private void setUpListeners() {
         Log.d(TAG, "setUpListeners() has been instantiated!");
         mSignOutButton.setOnClickListener(this);
+        codePicker.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                Log.d(TAG, "Saving the country name to the preference file");
+                SharedPreferences sharedPref = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+                Editor editor = sharedPref.edit();
+                String countryCode = codePicker.getSelectedCountryCode();
+                editor.putString(PREF_COUNTRY_CODE, countryCode);
+                editor.apply();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() has been instantiated!");
+        Log.d(TAG, "restoring the saved country name from the preference file");
+        SharedPreferences sharedPref = getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        String countryCode = sharedPref.getString(PREF_COUNTRY_CODE, "");
+        codePicker.setCountryForPhoneCode(Integer.valueOf(countryCode));
+
     }
 
     @Override
